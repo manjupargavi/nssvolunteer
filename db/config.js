e66f777ee0d6597bae5498e4714c9927c449482d
manjupargavi <manjupargavis.21aid@kongu.edu>
2023-06-17 11:23:31 +0530

    Commit message

--- db/config.js
+++ db/config.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/nss");
+
+// Get the MongoDB URI from the environment variable or use a default value
+const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nss";
+
+// Connect to MongoDB using the provided URI
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your application or perform further operations
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
+    // Handle the error appropriately
+  });
